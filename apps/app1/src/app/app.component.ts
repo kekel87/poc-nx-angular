d55294127e0afba363686f221b8aaa002a1b2a87
Michael Parry <michael.parry.87@gmail.com>
2022-02-17 17:23:33 +0100

    chore: nx create workspace
    - `npx create-nx-workspace@latest`

---
+++ apps/app1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poc-nx-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app1';
+}
